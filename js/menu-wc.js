c9155c19d6929b317fe7d624c96e149348e370f9
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2020-06-13 02:40:32 +0000

    docs: update 2020-06-13T02:40:31.105Z

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -86,6 +86,9 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                     <li class="link ">
                                         <a href="additional-documentation/input-rules.html" data-type="entity-link" data-context-id="additional">Input Rules</a>
                                     </li>
+                                    <li class="link ">
+                                        <a href="additional-documentation/reactive-forms.html" data-type="entity-link" data-context-id="additional">Reactive Forms</a>
+                                    </li>
                         </ul>
                     </li>
                     <li class="chapter modules">
